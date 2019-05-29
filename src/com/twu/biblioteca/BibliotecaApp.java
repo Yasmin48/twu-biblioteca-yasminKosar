1e8dc3b21eb96b32901106b750a4c679953f51c9
yasmin48 <yasmin_koser@hotmail.com>
2019-05-29 20:48:40 +0100

    add library class

--- src/com/twu/biblioteca/BibliotecaApp.java
+++ src/com/twu/biblioteca/BibliotecaApp.java
@@ -9,5 +9,33 @@ public class BibliotecaApp {
 
         System.out.println();
 
+        Library library = new Library();
+
+        //System.out.println(library.getBooks());
+
+        //System.out.println(library);
+
+         Book testBook = new Book("Man's Search for Meaning", "Viktor Frankl", 1946);
+
+         String response = library.checkoutBook(testBook);
+        //System.out.println(response); // checked out book not showing on the list;
+        //System.out.println(library);
+
+//        Book invalidBook = new Book("Harry Potter", "J K Rowling", 2007);
+//        String result = library.checkoutBook(invalidBook);
+//        System.out.println(result);
+
+//         Book returnedBook = new Book("Yasmin", "test", 1999);
+//         String returnResponse = library.returnBook(returnedBook);
+//         System.out.println(library);
+//         System.out.println(returnResponse);
+
+         Book book = new Book("Man's Search for Meaning", "Viktor Frankl", 1946);
+         String validReturn = library.returnBook(book);
+         System.out.println(library);
+         System.out.println(validReturn);
+
+
+
     }
 }
