68151450f29143514779dd83cc55958d8c0c32ea
yasmin48 <yasmin_koser@hotmail.com>
2019-05-29 18:08:49 +0100

    Add Book class and tests

--- src/com/twu/biblioteca/BibliotecaApp.java
+++ src/com/twu/biblioteca/BibliotecaApp.java
@@ -5,6 +5,9 @@ public class BibliotecaApp {
     public static void main(String[] args) {
 
 
-        System.out.println("Hello, world!");
+        System.out.println("Welcome to Biblioteca. Your one-stop-shop for great book titles in Bangalore!");
+
+        System.out.println();
+
     }
 }
---
+++ test/com/twu/biblioteca/BookTest.java
@@ -0,0 +1,43 @@
+package com.twu.biblioteca;
+
+import org.junit.Test;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+
+
+
+
+public class BookTest {
+
+    String testTitle = "All About Love";
+    String testAuthor = "Bell Hooks";
+    int testYear = 2000;
+
+    Book testBook = new Book(testTitle, testAuthor, testYear);
+
+    @Test
+    public void getsTitleOfBook(){
+       assertThat(testBook.getTitle(), is(equalTo(testTitle)));
+    }
+
+    @Test
+    public void getsAuthorOfBook() {
+        assertThat(testBook.getAuthor(), is(equalTo(testAuthor)));
+    }
+
+    @Test
+    public void getsYearOfBook(){
+        assertThat(testBook.getYear(), is(equalTo(testYear)));
+    }
+
+    @Test
+    public void testToStringMethod(){
+
+        String expectedResult = "All About Love Bell Hooks 2000";
+
+        assertThat(testBook.toString(), is(equalTo(expectedResult)));
+    }
+}
+
